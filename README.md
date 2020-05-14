31dd02030224d862466131291c90bdd75b5bfc78
Matteo Landi <matteo@matteolandi.net>
2020-05-14 08:58:36 +0200

    Cosmetic changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lg
+
+## Changelog
+
+0.0.1 (2020-05-14)
+
+- First release (already functional)
--- src/main.lisp
+++ src/main.lisp
@@ -4,6 +4,8 @@
 ;; https://gist.github.com/gruber/249502
 (defvar *link-regexp* "(?i)\\b((?:[a-z][\\w-]+:(?:/{1,3}|[a-z0-9%])|www\\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
 
+; Options -----------------------------------------------------------------------------------------
+
 (opts:define-opts
   (:name :help
          :description "print the help text and exit"
@@ -32,17 +34,16 @@
         (format T "~a~%" *version*)
         (opts:exit)))))
 
+; API ---------------------------------------------------------------------------------------------
+
 (defun grab-links (s)
   (cl-ppcre:all-matches-as-strings *link-regexp* s))
 
-(defun process-input ()
+(defun toplevel ()
+  (parse-opts)
   (loop
     :for line = (read-line NIL NIL :eof)
     :until (eq line :eof)
     :do (loop
           :for link :in (grab-links line)
           :do (format T "~a~%" link))))
-
-(defun toplevel ()
-  (parse-opts)
-  (process-input))
